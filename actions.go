1560d9f2b35e2cd479eb305947561e3a7965b088
Andrei Avram <avramandrei@ymail.com>
2017-01-02 14:00:06 +0200

    Add list repositories action.

--- actions.go
+++ actions.go
@@ -2,10 +2,48 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/go-github/github"
 )
 
 func ListAction(config *Config) {
-	fmt.Println(config)
+	client := NewGithubClient(config.Token)
+	page := 1
+
+	Loop:
+	for ;; {
+		o := &github.RepositoryListOptions{
+			Sort: config.Repository.ListSortBy,
+			Type: config.Repository.ListFilterByType,
+			ListOptions: github.ListOptions{
+				Page: page,
+				PerPage: config.Repository.ListPerPage,
+			},
+		}
+		repos, resp, err := client.Repositories.List(config.Username, o)
+
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+
+		for _, repo := range repos {
+			fmt.Printf("\n%s", *repo.Name)
+		}
+
+		if resp.NextPage == 0 {
+			break Loop
+		}
+
+		option := readInput(labels.NextPageOrSkip, options.NextPage, false)
+		switch option {
+		case options.Skip:
+			break Loop
+		default:
+			page++
+		}
+	}
+
+	fmt.Println()
 }
 
 func UpdateAction(config *Config) {
